b33b2ec889d48fda17b82ef3020bd650593b3aae
RealGregor <gb0400@student.uni-lj.si>
2020-02-27 17:14:39 +0000

    Added Gravitacija.java

---
+++ v1/Gravitacija.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Gravitacija {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int visina = sc.nextInt();
+		final double C = 6.674* (Math.pow(10, -11));
+		final double M = 5.972* (Math.pow(10, 24));
+		final double r = 6.371*(Math.pow(10, 6));
+		double a = (C*M)/((r+visina)*(r+visina));
+		System.out.println(a);
+	}
+
+}
